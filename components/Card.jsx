86af557358d941b4ff2f5ce54a474236a7d86f2f
Will <will@chorus.one>
2023-06-24 13:27:17 -0500

    randomish slide in animation

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,17 +1,16 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import { data } from 'autoprefixer';
+import PropTypes from "prop-types";
+import React from "react";
+import { data } from "autoprefixer";
 
 export const Card = ({ card }) => {
   const { id, reg, ital } = card;
 
   return (
-    <div className="flex-col mx-auto flex rounded-3xl items-center justify-center p-4 h-full max-h-[347px] animate-[wiggle_10s_ease_infinite]  bg-white text-j-blue max-w-[430px] drop-shadow-[0px_25px_50px_rgba(0,0,0,0.4)]">
-      <p className="font-fruit font-normal mx-auto text-4xl tracking-tighter align-middle items-center text-center p-16 m-8">
-        {reg}
-        <i className="font-thin"> {ital}</i>.
+    <div className="z-20 card mx-auto flex rounded-[50px] items-center justify-center p-4 h-[296px] max-h-[296px] animate-[wiggle_10s_ease_infinite] bg-white text-j-blue w-[420px] max-w-[420px] drop-shadow-[0px_25px_50px_rgba(0,0,0,0.4)]">
+      <p className="font-fruit font-normal mx-auto text-4xl tracking-tight align-middle items-center text-center p-16 m-8">
+        {reg} <i className="font-thin">{ital}</i>.
       </p>
-      <p className="uppercase text-center font-mont tracking-tight font-normal text-xs">
+      <p className="flex uppercase text-center absolute bottom-6 font-mont tracking-tight font-normal text-xs">
         song seeds by jacob&#39;s blue
       </p>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,39 @@
-import Image from 'next/image';
-import React from 'react';
-import useSWR from 'swr';
-import { Card } from '../../components/Card';
-import { ReactFragment } from 'react';
+import Image from "next/image";
+import React from "react";
+import useSWR from "swr";
+import { Card } from "../../components/Card";
+import { ReactFragment } from "react";
 // import { getRandomCard } from '../cards';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Home() {
   const [activeCard, setActiveCard] = React.useState(0);
+  const [nextCard, setNextCard] = React.useState(1);
 
   const getRandomCard = (e) => {
     // Add a class to the current card to animate it off the screen
-    document.querySelector('.card').classList.add('card-exit');
+    const active = document.querySelector("#active-card");
+    active.classList.replace("transition-none", "transition-all");
+    active.classList.replace("left-1/2", "-left-[280px]");
 
-    const len = data.length;
-    setActiveCard(Math.floor(Math.random() * len));
+    const next = document.querySelector("#next-card");
+    next.classList.replace("transition-none", "transition-all");
+    next.classList.replace("left-[calc(100%+280px)]", "left-1/2");
+
+    /* const len = data.length; */
+    /* setActiveCard(Math.floor(Math.random() * len)); */
 
     // Wait for a short time before removing the exit animation class
     setTimeout(() => {
-      document.querySelector('.card').classList.remove('card-exit');
-    }, 1000);
+      setActiveCard(nextCard);
+      setNextCard(Math.floor(Math.random() * data.length));
+      active.classList.replace("transition-all", "transition-none");
+      active.classList.replace("-left-[280px]", "left-1/2");
+
+      next.classList.replace("transition-all", "transition-none");
+      next.classList.replace("left-1/2", "left-[calc(100%+280px)]");
+    }, 600);
   };
 
   // const handleClick = () => {
@@ -37,7 +50,7 @@ export default function Home() {
   //   }, 500);
   // };
 
-  const { data, error } = useSWR('/api/cards', fetcher);
+  const { data, error } = useSWR("/api/cards", fetcher);
 
   const main = () => {
     if (error) {
@@ -47,10 +60,10 @@ export default function Home() {
       return <main className="container-fg text-xl py-20">loading...</main>;
     }
     return (
-      <main className="flex min-h-screen items-center justify-center p-24 font-mont font-thin mx-auto">
-        <div className="w-full max-w-5xl flex-block mx-auto">
+      <main className="flex min-h-screen items-center justify-center py-24 font-mont font-thin mx-auto">
+        <div className="w-full flex-block mx-auto">
           <div className="uppercase w-full flex justify-center mx-auto">
-            song seeds by{' '}
+            song seeds by{" "}
             <a
               className="underline text-blue-200 hover:text-white transition-all transition-.2ms"
               href="https://jacobs.blue"
@@ -58,26 +71,21 @@ export default function Home() {
               jacob&#39;s blue
             </a>
           </div>
-          <div className="flex flex-wrap items-center col-span-1 justify-center mx-auto py-52 space-y-16 w-full">
-            <div className="card mx-auto flex rounded-[50px] items-center justify-center p-4 h-[296px] max-h-[296px] animate-[wiggle_10s_ease_infinite]  bg-white text-j-blue w-[420px] max-w-[420px] drop-shadow-[0px_25px_50px_rgba(0,0,0,0.4)]">
-              <p className="font-fruit font-normal mx-auto text-4xl tracking-tight align-middle items-center text-center p-16 m-8">
-                {data[activeCard].reg}{' '}
-                <i className="font-thin">{data[activeCard].ital}</i>.
-              </p>
-              <p className="flex uppercase text-center absolute bottom-6 font-mont tracking-tight font-normal text-xs">
-                song seeds by jacob&#39;s blue
-              </p>
+          <div className="flex flex-wrap items-center col-span-1 justify-center mx-auto py-32 space-y-16 w-full overflow-hidden">
+            <div className="w-full relative h-[320px] z-10">
+              <div
+                id="active-card"
+                className="z-20 absolute top-0 left-1/2 transition-all duration-500 -translate-x-1/2"
+              >
+                <Card card={data[activeCard]} />
+              </div>
+              <div
+                id="next-card"
+                className="absolute top-0 left-[calc(100%+280px)] transition-all duration-500 -translate-x-1/2"
+              >
+                <Card card={data[nextCard]} />
+              </div>
             </div>
-
-            {/* Was trying to use old function from JB */}
-            {/* {data.map(function (card, index) {
-              if (card.id > 0) return <Card key={index} card={card} />;
-            })} */}
-            {/* <Card
-              reg={data[activeCard].reg}
-              ital={data[activeCard].ital}
-              id={data[activeCard].id}
-            /> */}
 
             <div className="flex w-full justify-center mx-auto">
               <button
