799bb7e5fe67d0af8228cd1c786dd4a46a02422b
Jake <jacob@liveineverynow.com>
2023-06-30 13:50:17 -0400

    Format?

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import React from "react";
-import useSWR from "swr";
-import { ReactFragment } from "react";
+import Image from 'next/image';
+import React from 'react';
+import useSWR from 'swr';
+import { ReactFragment } from 'react';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -24,13 +24,13 @@ export default function Home() {
   const [count, setCount] = React.useState(0);
 
   const getRandomCard = (e) => {
-    const cards = document.querySelector("#cards");
+    const cards = document.querySelector('#cards');
     const curr = cards.children[0];
     const next = cards.children[1];
 
     if (next) {
-      curr.classList.replace("left-1/2", "-left-[280px]");
-      next.classList.replace("left-[calc(100%+280px)]", "left-1/2");
+      curr.classList.replace('left-1/2', '-left-[280px]');
+      next.classList.replace('left-[calc(100%+280px)]', 'left-1/2');
 
       setTimeout(() => {
         curr.remove();
@@ -38,23 +38,23 @@ export default function Home() {
         let r = Math.floor(Math.random() * data.length);
         console.log(r, data.length);
         let randCard = data[r];
-        let container = document.createElement("div");
+        let container = document.createElement('div');
         container.className =
-          "absolute w-[90%] top-0 left-[calc(100%+280px)] transition-all duration-500 -translate-x-1/2";
-        let card = document.createElement("div");
+          'absolute w-[90%] top-0 left-[calc(100%+280px)] transition-all duration-500 -translate-x-1/2';
+        let card = document.createElement('div');
         card.className =
-          "card mx-auto flex w-full rounded-[50px] items-center justify-center p-4 h-[296px] max-h-[296px] animate-[wiggle_10s_ease_infinite] bg-white text-j-blue w-[420px] max-w-[420px] drop-shadow-[0px_25px_50px_rgba(0,0,0,0.4)]";
-        let text = document.createElement("p");
+          'card mx-auto flex w-full rounded-[50px] items-center justify-center p-4 h-[296px] max-h-[296px] animate-[wiggle_10s_ease_infinite] bg-white text-j-blue w-[420px] max-w-[420px] drop-shadow-[0px_25px_50px_rgba(0,0,0,0.4)]';
+        let text = document.createElement('p');
         text.textContent = randCard.reg;
         text.className =
-          "font-fruit font-normal mx-auto text-4xl tracking-tight align-middle items-center text-center p-16 m-8";
-        let ital = document.createElement("i");
-        ital.className = "font-thin pl-2";
-        ital.textContent = randCard.ital + ".";
+          'font-fruit font-normal mx-auto text-4xl tracking-tight align-middle items-center text-center p-16 m-8';
+        let ital = document.createElement('i');
+        ital.className = 'font-thin pl-2';
+        ital.textContent = randCard.ital + '.';
         text.appendChild(ital);
-        let footer = document.createElement("p");
+        let footer = document.createElement('p');
         footer.className =
-          "flex uppercase text-center absolute bottom-6 font-mont tracking-tight font-normal text-xs";
+          'flex uppercase text-center absolute bottom-6 font-mont tracking-tight font-normal text-xs';
         footer.textContent = "song seeds by jacob's blue";
         card.appendChild(text);
         card.appendChild(footer);
@@ -64,7 +64,7 @@ export default function Home() {
     }
   };
 
-  const { data, error } = useSWR("/api/cards", fetcher);
+  const { data, error } = useSWR('/api/cards', fetcher);
 
   const main = () => {
     if (error) {
@@ -76,7 +76,7 @@ export default function Home() {
     return (
       <main className="flex flex-col min-h-screen items-center justify-center py-24">
         <div className="uppercase w-full flex justify-center mx-auto">
-          song seeds by{" "}
+          song seeds by{' '}
           <a
             className="underline text-blue-200 hover:text-white transition-all transition-.2ms"
             href="https://jacobs.blue"
